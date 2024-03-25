6aa48948957012bb89d2c4ba66f36160d50c17d5
Carlos Maria Canut Dominguez <canutdominguez@gmail.com>
2024-03-26 00:50:50 +0100

    update: projects deleted to check issues

--- pages/index.js
+++ pages/index.js
@@ -138,7 +138,7 @@ export default function Home({ translations }) {
                 <Scene translations={translations} />
             </section>
 
-            <section className='flex flex-col gap-24 w-[100dvw]'>
+            {/* <section className='flex flex-col gap-24 w-[100dvw]'>
                 <div className='flex flex-col w-[100dvw] items-center hover:ease-in'>
                     <motion.div
                         initial={{ opacity: 0, y: -10 }}
@@ -172,7 +172,7 @@ export default function Home({ translations }) {
                         <ProjectCard title={'LOLPICKS'} onClick={() => router.push('/projects/lolpicks')} image_url={'/images/lolpicks.png'} github_link={'https://github.com/CarlosCanut'} />
                     </motion.div>
                 </div>
-            </section>
+            </section> */}
 
 
 
